e194fc257b35258f60721c1a54d90a153ff1f31a
Parth Dalal <parthdalal06@gmail.com>
2022-09-13 17:55:01 +0530

    Create cssmodules.js

---
+++ session4/cssmodules.js
@@ -0,0 +1,26 @@
+import styles from './styles.module.css';
+export default function App() {
+  return (
+    <section className={styles.testimonial}>
+      <div className={styles['testimonial-wrapper']}>
+        <img
+          src="https://randomuser.me/api/portraits/women/48.jpg"
+          alt="Tammy Stevens"
+          className={styles['testimonial-avatar']}
+        />
+<p className={styles['testimonial-name']}>
+          Parth Dalal
+          <span> · Front End Developer</span>
+        </p>
+
+      </div>
+    </section>
+  );
+}
+
+
+
+<p class="_styles__testimonial-name_309571057">
+  Parth Dalal 
+</p>
+
